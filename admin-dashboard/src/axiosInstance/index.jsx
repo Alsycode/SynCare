cf4e8fc898ded9d5c30478f7e4ec13e3b881e3ea
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-11-05 19:45:01 +0530

    fix backend error

--- admin-dashboard/src/axiosInstance/index.jsx
+++ admin-dashboard/src/axiosInstance/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://syncare.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
